0daffd16d8f1afef94f68159fde3602faada1c70
solleedata <solleedata@gmail.com>
2022-04-18 23:39:12 +0900

    create crypto wallet app

---
+++ navigation/tabs.js
@@ -0,0 +1,106 @@
+import React from "react";
+import {
+  TouchableOpacity,
+} from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+
+import { Home, Portfolio, Market, Profile } from "../screens"
+import { TabIcon } from '../components'
+import { COLORS, icons } from "../constants"
+
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        // showLabel: false,
+        style: {
+          height: 140,
+          backgroundColor: COLORS.primary,
+          borderTopColor: "transparent",
+        }
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          TabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                icon={icons.home}
+                label="Home"
+              />
+            )
+          }
+        }}
+      />
+      <Tab.Screen
+        name="Portfolio"
+        component={Portfolio}
+        options={{
+          TabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                icon={icons.portfolio}
+                label="Porfolio"
+              />
+            )
+          }
+        }}
+      />
+      <Tab.Screen
+        name="Trade"
+        component={Home}
+        options={{
+          TabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                icon={icons.trade}
+                label="Trade"
+                isTrade={true}
+              />
+            )
+          }
+        }}
+      />
+      <Tab.Screen
+        name="Market"
+        component={Market}
+        options={{
+          TabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                icon={icons.market}
+                label="Market"
+              />
+            )
+          }
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          TabBarIcon: ({ focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                icon={icons.profile}
+                label="Profile"
+              />
+            )
+          }
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+export default Tabs;
