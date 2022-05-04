d670d3dafc653a6d28cdf6c46f7c2ca9ad3bd15b
solleedata <solleedata@gmail.com>
2022-05-04 09:18:35 +0900

    FEAT: mmarket Actions

--- stores/market/marketActions.js
+++ stores/market/marketActions.js
@@ -76,4 +76,41 @@ export function getHoldings(holdings = [], currency = "usd", orderBy = "market_c
   }
 }
 
-// 50.56
+// COIN MARKET
+export const getCoinMarketBegin = () => ({
+  type: GET_COIN_MARKET_BEGIN
+})
+
+export const getCoinMarketSuccess = () => ({
+  type: GET_COIN_MARKET_SUCCESS,
+  payload: { coins }
+})
+
+export const getCoinMarketFailure = () => ({
+  type: GET_COIN_MARKET_FAILURE,
+  payload: { error }
+})
+
+export function getCoinMarket(currency = "usd", orderBy = "market_cap_desc", sparkline = true, priceChangePerc = "7d", perPage = 10, page = 1) {
+  return dispatch => {
+    dispatch(getCoinMarketBegin())
+
+    let apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=${orderBy}&per_page=${perPage}&page=${page}&sparkline=${sparkline}&price_change_percentage=${priceChangePerc}&ids=${ids}`
+
+    return axios({
+      url: apiUrl,
+      method: 'GET',
+      header: {
+        Accept: "application/json"
+      }
+    }).then((response) => {
+      if (response.state == 200) {
+        dispatch(getCoinMarketSuccess(response.data))
+      } else {
+        dispatch(getCoinMarketFailure(response.data))
+      }
+    })
+  }
+}
+
+// 56.25
